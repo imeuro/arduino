09f04a64a698d2af07ac5709e299279c1e8e6b58
Mauro Fioravanzi <imeuro@users.noreply.github.com>
2016-02-11 17:51:33 +0100

    some explaining

--- temperINO_souces/checkTemp.py
+++ temperINO_souces/checkTemp.py
@@ -1,3 +1,8 @@
+# checks temp from /dev/ttyACM0 every 30s,
+# writes temp to file so it can be displayed in web app.
+# It is not elegant at all. I know.
+# Possibly it will be replaced by an on demand connection to serial via php
+
 import serial
 Ino = serial.Serial('/dev/ttyACM0', 9600, timeout=30)
 
