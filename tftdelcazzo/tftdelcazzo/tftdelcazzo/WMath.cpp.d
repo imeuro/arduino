1d000f1897b1e3421215a99915698f85d27c2017
imeuro <mauro.fioravanzi@gmail.com>
2016-03-17 12:33:47 +0100

    riordinato funzioni

---
+++ tftdelcazzo/tftdelcazzo/tftdelcazzo/WMath.cpp.d
@@ -0,0 +1,2 @@
+/media/removable/UNTITLED/Arduino/sketches/tftdelcazzo/tftdelcazzo/WMath.cpp.o: \
+ /usr/share/arduino/hardware/arduino/cores/robot/WMath.cpp
---
+++ tftdelcazzo/tftdelcazzo/tftdelcazzo/new.cpp.d
@@ -0,0 +1,3 @@
+/media/removable/UNTITLED/Arduino/sketches/tftdelcazzo/tftdelcazzo/new.cpp.o: \
+ /usr/share/arduino/hardware/arduino/cores/robot/new.cpp \
+ /usr/share/arduino/hardware/arduino/cores/robot/new.h
