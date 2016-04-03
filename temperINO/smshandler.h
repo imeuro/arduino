241323143b1461759da3aab64b8e4f23a3afec4a
imeuro <mauro.fioravanzi@gmail.com>
2016-04-03 20:12:18 +0200

    tacchette + risparmione strings

--- temperINO/smshandler.h
+++ temperINO/smshandler.h
@@ -1,6 +1,5 @@
 void ProcessMsg() {
-String smscontent;
-String prefix = "Mergozzo";
+char* smscontent;
 char* SMSreplytxt;
 char* SMSreplynum;
 //String fullSMS;
@@ -9,42 +8,36 @@ char* SMSreplynum;
     SMSreplynum = cell.Sender();
     SMSreplytxt = "";
 
-    if (smscontent == prefix+" OFF" || smscontent == prefix+" off") {
+    if (smscontent == "Mergozzo OFF" || smscontent == "Mergozzo off") {
       set_heat_prog(0);
       mainUI(0);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"OFF\".";
+      SMSreplytxt = "Caldaia Mergozzo impostata su \"OFF\". \nTemp: ";
     }
-    else if (smscontent == prefix+" T1" || smscontent == prefix+" t1") {
+    else if (smscontent == "Mergozzo T1" || smscontent == "Mergozzo t1") {
       set_heat_prog(1);
       mainUI(1);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"T1\".";
+      SMSreplytxt = "Caldaia Mergozzo impostata su \"T1\". \nTemp: ";
     }
-    else if (smscontent == prefix+" T2" || smscontent == prefix+" t2") {
+    else if (smscontent == "Mergozzo T2" || smscontent == "Mergozzo t2") {
       set_heat_prog(2);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"T2\".";
+      SMSreplytxt = "Caldaia Mergozzo impostata su \"T2\". \nTemp: ";
       mainUI(2);
     }
-    else if (smscontent == prefix+" T3" || smscontent == prefix+" t3") {
+    else if (smscontent == "Mergozzo T3" || smscontent == "Mergozzo t3") {
       set_heat_prog(3);
       mainUI(3);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"T3\".";
+      SMSreplytxt = "Caldaia Mergozzo impostata su \"T3\". \nTemp: ";
     }
-    else if (smscontent == prefix+" AUTO" || smscontent == prefix+" auto") {
+    else if (smscontent == "Mergozzo AUTO" || smscontent == "Mergozzo auto") {
       set_heat_prog(9);
       mainUI(9);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"AUTO\".";
+      SMSreplytxt = "Caldaia Mergozzo impostata su \"AUTO\". \nTemp: ";
     }
     else {
       SMSreplytxt = "Comando non riconosciuto (es. \"Mergozzo T2\" per impostare a T2 il termostato).";
     }
 
-    //sprintf(fullSMS,"%s \nTempertaura attuale:  %f",SMSreplytxt,tempC);
-    //fullSMS = String(SMSreplytxt)+String("\nTempertaura attuale: ")+String(tempC);
-    //char charSMS[161];
-    //fullSMS.toCharArray(charSMS,161);
-    
-    Serial.println(tempC);
-    delay(2000);
+    dtostrf(tempC, 4, 1, &SMSreplytxt[80]);
 
     //send sms confirmation to sender number
     cell.Rcpt(SMSreplynum);
