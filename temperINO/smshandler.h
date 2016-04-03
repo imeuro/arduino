c37120ee5103095518bc96dfb9e21bbe3c982ece
imeuro <mauro.fioravanzi@gmail.com>
2016-04-03 21:04:30 +0200

    ..

--- temperINO/smshandler.h
+++ temperINO/smshandler.h
@@ -1,5 +1,5 @@
 void ProcessMsg() {
-char* smscontent;
+String smscontent;
 char* SMSreplytxt;
 char* SMSreplynum;
 
@@ -10,32 +10,33 @@ char* SMSreplynum;
     if (smscontent == "Mergozzo OFF" or smscontent == "Mergozzo off") {
       set_heat_prog(0);
       mainUI(0);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"OFF\".\nTemp: ";
+      SMSreplytxt = "Caldaia impostata su \"OFF\".\nTemp: ";
     }
     else if (smscontent == "Mergozzo T1" or smscontent == "Mergozzo t1") {
       set_heat_prog(1);
       mainUI(1);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"T1\".\nTemp: ";
+      SMSreplytxt = "Caldaia impostata su \"T1\".\nTemp: ";
     }
     else if (smscontent == "Mergozzo T2" or smscontent == "Mergozzo t2") {
       set_heat_prog(2);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"T2\".\nTemp: ";
+      SMSreplytxt = "Caldaia impostata su \"T2\".\nTemp: ";
       mainUI(2);
     }
     else if (smscontent == "Mergozzo T3" or smscontent == "Mergozzo t3") {
       set_heat_prog(3);
       mainUI(3);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"T3\".\nTemp: ";
+      SMSreplytxt = "Caldaia impostata su \"T3\".\nTemp: ";
     }
     else if (smscontent == "Mergozzo AUTO" or smscontent == "Mergozzo auto") {
       set_heat_prog(9);
       mainUI(9);
-      SMSreplytxt = "Caldaia Mergozzo impostata su \"AUTO\".\nTemp: ";
+      SMSreplytxt = "Caldaia impostata su \"AUTO\".\nTemp: ";
     }
     else {
       SMSreplytxt = "Comando non riconosciuto (es. \"Mergozzo T2\").";
     }
 
+    Serial.println(tempC);
     dtostrf(tempC, 4, 1, &SMSreplytxt[50]);
     delay(2000);
 
