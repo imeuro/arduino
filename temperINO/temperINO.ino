cce237369678386447f042cada4451944f48ce1f
imeuro <mauro.fioravanzi@gmail.com>
2016-04-29 15:40:38 +0200

    back to work!

--- temperINO/temperINO.ino
+++ temperINO/temperINO.ino
@@ -21,33 +21,48 @@
 void setup(void) {
 
   Serial.begin(9600);
-  //Wire.begin();
+  Wire.begin();
   tft.begin();
-  
-  //tft.setRotation(1);
-  tft.fillScreen(ILI9341_BLACK);
+  if (!ts.begin()) {
+    Serial.println("Couldn't start touchscreen controller");
+    while (1);
+  }
+  tft.setRotation(1);
+  tft.fillScreen(BLACK);
 
-  //mainUI(9);              // at startup goes in AUTO prog
-  //set_heat_prog(9);       // at startup goes in AUTO prog
+  mainUI(9);              // at startup goes in AUTO prog
+  set_heat_prog(9);       // at startup goes in AUTO prog
   
-  //cell.begin(9600);
-  //cell.Verbose(true);
-  //cell.Boot(); 
-  //cell.FwdSMS2Serial();
-
+  cell.begin(9600);
+  cell.Verbose(true);
+  cell.Boot(); 
+  cell.FwdSMS2Serial();
+  Serial.flush();
 }
 
 void loop(void) {
-  
+ // See if there's any  touch data for us
+  if (ts.bufferEmpty()) {
+    return;
+  }
+  /*
+  // You can also wait for a touch
+  if (! ts.touched()) {
+    return;
+  }
+  */
+
+  // Retrieve a point  
   TS_Point p = ts.getPoint();
+
   // Scale from ~0->4000 to tft.width using the calibration #'s
   p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
   p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
 
   
-  Serial.print("("); Serial.print(p.x);
-  Serial.print(", "); Serial.print(p.y);
-  Serial.println(")");
+  //Serial.print("("); Serial.print(p.x);
+  //Serial.print(", "); Serial.print(p.y);
+  //Serial.println(")");
   
 
   if (timeElapsed > interval) {
@@ -60,14 +75,14 @@ void loop(void) {
   }
 
 
-  //if (cell.ReceiveSMS()){
-  //  ProcessMsg();
-  //}
+  if (cell.ReceiveSMS()){
+    ProcessMsg();
+  }
   
     
     // OFF,AUTO o MAN
-    /*if (p.x < 130) { // fascia bassa
-      if (p.y < 80 && MANmenuOpen == 0) { // MAN
+    if (p.x < 130) { // fascia bassa
+      if (p.y >160 && MANmenuOpen == 0) { // MAN
         tft.fillRect(220, 170, 90, 50, YELLOW);
         tft.setCursor(248, 188);
         tft.setTextColor(BLACK);  tft.setTextSize(2);
@@ -85,7 +100,7 @@ void loop(void) {
         mainUI(9);
         set_heat_prog(9);
       }
-      else if (p.y > 160) { //OFF
+      else if (p.y < 80) { //OFF
         tft.fillRect(10, 170, 90, 50, WHITE);
         tft.setCursor(38, 188);
         tft.setTextColor(BLACK);  tft.setTextSize(2);
@@ -97,21 +112,21 @@ void loop(void) {
     else if (MANmenuOpen == 1) {
       if (p.x > 110 && p.x < 240) {
         tft.setTextColor(BLACK);  tft.setTextSize(2);
-        if (p.y > 160) { // T1
-          mainUI(1);
-          set_heat_prog(1);
+        if (p.y > 160) { // T3
+          mainUI(3);
+          set_heat_prog(3);
         }
         else if (p.y >= 80 && p.y <= 160) { // T2
           mainUI(2);
           set_heat_prog(2);
         }
-      else if (p.y < 80) { // T3
-          mainUI(3);
-          set_heat_prog(3);
+      else if (p.y < 80) { // T1
+          mainUI(1);
+          set_heat_prog(1);
         }
       }
     }
-  */  
+  
 
 
 }
